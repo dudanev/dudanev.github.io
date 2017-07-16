b6b394dd673b0f2d7cf68fd6b26733b1c3b06f10
betodasilva <jrs_robert@hotmail.com>
2017-07-16 17:28:08 -0300

    add home layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   
-	
+	var mnsry = new Masonry('.works-grid', {
+		itemSelector: '.work-item',
+		columnWidth: '.work-sizer',
+		fitWidth: true,
+
+	});
 
 
 
 
-  
+
 });
