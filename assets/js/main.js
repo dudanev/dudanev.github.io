6127653cc69dacb9fa1bf26c11c32fcc4995f4a7
betodasilva <jrs_robert@hotmail.com>
2017-07-19 23:07:25 -0300

    add column feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,105 @@
 
 document.addEventListener("DOMContentLoaded", function() {
   
-  	var grid, item;
 
-  	grid = document.querySelector('.works-grid');
-  	item = document.querySelectorAll('.work-item');
+  	(function homeGrid(){
+  		  	var grid, item;
 
-	imagesLoaded( grid, function() {
+  		  	grid = document.querySelector('.works-grid');
+  		  	item = document.querySelectorAll('.work-item');
 
-		for (let i = 0; i < item.length; i++) {
-			item[i].classList.add('img-loaded');	
+  		  	if (grid) {
+	  			imagesLoaded( grid, function() {
+
+	  				for (let i = 0; i < item.length; i++) {
+	  					item[i].classList.add('img-loaded');	
+	  				}
+	  				
+
+
+	  				var mnsry = new Masonry('.works-grid', {
+	  					itemSelector: '.work-item',
+	  					columnWidth: '.work-sizer',
+	  					fitWidth: true,
+
+	  				});
+
+	  			});
+  			}
+  	})();
+  	
+
+
+
+	/* CREATE COLUMN TEXT */
+	(function createColumnText(){
+
+		var isTrue = document.getElementsByName('column-text')[0].value;
+		var colOne = "coluna_um!",
+			colTwo = "coluna_dois!";
+
+
+		// console.log(isTrue);
+
+		if(isTrue) {
+
+			$('.wrk__content p').each(function (){
+				var _ = $(this);
+				var found = false;
+				var newColOne, newColTwo;
+				// console.log(colOne);
+				var index = _.text().indexOf(colOne);
+				
+				// console.log(index);
+
+				if(index > -1) {
+					// console.log(index);
+					_.addClass('txt-col txt-col__one');
+					var txt = _.text();
+
+					txt = txt.slice(colOne.length, txt.length);
+					// console.log(txt, colOne.length );
+					_.text(txt)
+					// console.log(_.text());
+					// _.wrap(container);
+					
+				}
+
+				index = _.text().indexOf(colTwo);
+				if (index > -1) {
+					_.addClass('txt-col txt-col__two');
+					var txt = _.text();
+
+					txt = txt.slice(colTwo.length, txt.length);
+					// console.log(txt, colOne.length );
+					_.text(txt)
+					// container.html(_);
+					// _.remove();
+					found = true;
+				}
+
+				if (found) {
+					var container = "<div class='txt-col-container'></div>";
+					$('.txt-col').wrapAll(container);
+				}
+
+				
+
+
+			});
+
+
+
+
+
+
 		}
-		
 
 
-		var mnsry = new Masonry('.works-grid', {
-			itemSelector: '.work-item',
-			columnWidth: '.work-sizer',
-			fitWidth: true,
 
-		});
+	})();
+	
 
-	});
 	
 	
 
